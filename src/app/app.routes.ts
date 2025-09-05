7579331259ef891824d3298bf521df4bbf4a0945
XiaoPongo <amaydeepp12510@gmail.com>
2025-09-05 21:51:14 +0530

    test 4.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard';
 import { StudentRegComponent } from './student-reg/student-reg.component';
 import { MentorRegComponent } from './mentor-reg/mentor-reg.component';
+import { DashboardComponent } from './dashboard/mentor-dashboard/mentor-dashboard.component';
 import { AuthGuard } from './auth.guard';
 
 export const appRoutes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'student-reg', component: StudentRegComponent },
   { path: 'mentor-reg', component: MentorRegComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'mentor-dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'login' }
 ];
