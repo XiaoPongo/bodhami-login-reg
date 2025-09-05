6f96048e2ddefb13788e3e7b7c00591a36e3c4c9
XiaoPongo <amaydeepp12510@gmail.com>
2025-09-06 00:57:57 +0530

    test 4.4

---
+++ src/app/dashboard/layout/mentor-layout/mentor-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mentor-layout',
+  imports: [],
+  templateUrl: './mentor-layout.html',
+  styleUrl: './mentor-layout.css'
+})
+export class MentorLayout {
+
+}
